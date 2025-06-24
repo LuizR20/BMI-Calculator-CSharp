b91a6e25bfe2f6d6f2dd3aa3b44f085e01426914
Luiz Raphael <luizraphael1001@gmail.com>
2025-06-23 23:09:09 -0300

    Criar bmicalculator.cs

---
+++ bmicalculator.cs
@@ -0,0 +1,46 @@
+using System;
+
+public class Calculator
+{
+  public static void Main (string[] args)
+  {
+    Console.WriteLine("=== IMC Calculator ===");
+    Console.WriteLine(" ");
+    Console.WriteLine("To start, please, enter your weight (Kg)");
+    double weight = Convert.ToDouble(Console.ReadLine().Replace(",", "."));
+    
+    Console.WriteLine(" ");
+    Console.WriteLine("Perfect, now, enter your height (m)");
+    double height = Convert.ToDouble(Console.ReadLine().Replace(",", "."));
+    
+    double IMC = weight / (height * height);
+    
+    Console.WriteLine(" ");
+    Console.WriteLine($"Your BMI is {IMC:F2}");
+   
+    if (IMC <= 18.5)
+    {
+      Console.WriteLine("Category: Underweight");
+    }
+    else if (IMC <= 24.9)
+    {
+      Console.WriteLine("Category: Normal weight");
+    }
+    else if (IMC <= 29.9)
+    {
+      Console.WriteLine("Category: Overweight");
+    }
+    else if (IMC <= 34.9)
+    {
+      Console.WriteLine("Category: Obesity (Grade 1)");
+    }
+    else if (IMC <= 39.9)
+    {
+      Console.WriteLine("Category: Obesity (Grade 2)");
+    }
+    else if (IMC > 39.9)
+    {
+      Console.WriteLine("Category: Obesity (Grade 3/ Morbid)");
+    }
+  }
+}
